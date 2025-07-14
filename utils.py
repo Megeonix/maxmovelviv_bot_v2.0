c50c023c005adb26f84e1c9ba32e3333e7190d01
Megeonix <121872157+Megeonix@users.noreply.github.com>
2025-07-14 17:36:52 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -43,3 +43,14 @@ OUT_CITY_PRICES = {
     "Фургон до 3 т гідроборт до 30м3": 27,
     "Фургон 5т гідроборт до 40м3": 27
 }
+from geopy.distance import geodesic
+
+# Центр Львова
+CITY_CENTER = (49.8419, 24.0315)
+CITY_RADIUS_KM = 10
+
+def is_within_city(location: tuple[float, float]) -> bool:
+    """
+    Перевіряє, чи координати входять у радіус 10 км від центру Львова
+    """
+    return geodesic(location, CITY_CENTER).km <= CITY_RADIUS_KM
