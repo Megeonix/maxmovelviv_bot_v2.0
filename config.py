2794e79dd93c460b8d6b64ebef43553141d3121e
Megeonix <121872157+Megeonix@users.noreply.github.com>
2025-07-14 10:51:24 +0300

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x]
+BOT_TOKEN = os.getenv("7976646211:AAGdsEcpvDl1-pzoyXEFc0lkmbm6tlUCKck")
+
+ADMINS = [
+    1364324881,  # Admin 1
+    591264759,   # Admin 2
+]
+
+Lviv_Center_Coords = (49.8419, 24.0315)
+Lviv_Boundary_km = 10  # умовна відстань від центру до межі Львова
+BASE_OUTSIDE_CITY_COST = 900
