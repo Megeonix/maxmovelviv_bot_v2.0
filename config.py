f60ebb9fbbe096284184b7acdddf444f62b5400b
Megeonix <121872157+Megeonix@users.noreply.github.com>
2025-07-15 10:43:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("7976646211:AAGdsEcpvDl1-pzoyXEFc0lkmbm6tlUCKck")
+BOT_TOKEN = os.getenv(7976646211:AAGdsEcpvDl1-pzoyXEFc0lkmbm6tlUCKck)
 ORS_API_KEY = os.getenv("5b3ce3597851110001cf6248aa59ec61e83c41059f923ebaff9a9868")
+ADMIN_CHAT_IDS = [1364324881, 591264759]
 
-LVIV_CENTER = (49.8419, 24.0315)  # Центр Львова
-LVIV_RADIUS_KM = 10  # Межа міста — 10 км
+LVIV_CENTER = (49.8419, 24.0315)
+CITY_RADIUS_KM = 10
