3435737c47ef752c554d632d2363506090d4a91f
Megeonix <121872157+Megeonix@users.noreply.github.com>
2025-07-15 10:42:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,20 @@
 import asyncio
 import logging
+import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from handlers import router
 from config import BOT_TOKEN
-from flask import Flask
-from threading import Thread
-
-bot = Bot(
-    token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-)
-dp = Dispatcher()
-dp.include_router(router)
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Бот працює!"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)
-
-def keep_alive():
-    t = Thread(target=run_flask)
-    t.start()
 
 async def main():
-    keep_alive()
     logging.basicConfig(level=logging.INFO)
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
